1bb1c12c990e154fd82759896021b80300cdabbe
Felipe Garrido <fgarridob.95@gmail.com>
2015-09-29 22:28:28 -0300

    Delete Preg1.m

--- Preg1.m
+++
@@ -1,6 +0,0 @@
-rect = @(t) abs(t) < (1/2);
-trian = @(t) (1 - abs(t)) .* (abs(t) < 1);
-gauss = @(t) exp(-pi * t.^2);
-esc = @(t) (t > 0);
-ramp = @(t) t .* (t > 0);
-sinc = @(t) sin(pi .* t) ./ (pi .* (t + (t == 0))) + (t == 0);
